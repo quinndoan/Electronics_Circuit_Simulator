f83bc6187e23f8de34efc716bd026fa663383ec1
Đoàn Thị Thu Quyên <thuquyentntl@gmail.com>
2024-06-08 16:52:50 +0700

    thêm trường hợp test

--- demo/src/main/java/demo/test/DCTestR.java
+++ demo/src/main/java/demo/test/DCTestR.java
@@ -1,27 +1,18 @@
 package demo.test;
-import demo.Components.Capacitor;
 import demo.Components.Complex;
 import demo.Components.EleController;
-import demo.Components.Inductor;
 import demo.Components.Resistor;
-public class DCTestRLC{
+
+public class DCTestR {
     public static void main(String[] args) {
         try {
             // Khởi tạo các phần tử trong mạch
             Resistor[] resistors = {
-                new Resistor(10), // 100 ohm resistor
-                new Resistor(20)  // 200 ohm resistor
-            };
-            Capacitor[] capacitors = {
-                new Capacitor(0.000001) // 1 µF capacitor
-            };
-            Inductor[] inductors = {
-                new Inductor(0.001) // 1 mH inductor
+                new Resistor(10) // 10 ohm resistor
             };
 
             // Khởi tạo EleController với các phần tử trong mạch
-            EleController eleController = new EleController(5.0, "DC", resistors, capacitors, inductors, 1); // 2 là mạch nối tiếp
-            
+            EleController eleController = new EleController(5.0, "DC", resistors, null, null, 1); // 1 là mạch nối tiếp
         
             double frequency = 0.0;
             Complex equivalentImpedance = eleController.getEquivalentImpedance(frequency);
