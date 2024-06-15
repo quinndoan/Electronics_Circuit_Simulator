9c17ae9fdf59380062dda93448b6b2e90223859d
Đoàn Thị Thu Quyên <thuquyentntl@gmail.com>
2024-06-15 23:11:04 +0700

    u

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module demo {
     requires javafx.controls;
     requires javafx.fxml;
-    requires deeplearning4j.core;
-    requires nd4j.nativeplatform;
 
     opens demo to javafx.fxml;
     exports demo;
